8cd6ecbabe6e4cd91518fc81c243df3956e6538c
YChen12345 <492898584@qq.com>
2023-08-20 21:09:38 +0800

    新增场景关卡

--- Assets/Scripts/Water.cs
+++ Assets/Scripts/Water.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class Water : MonoBehaviour
 {
+    public GameObject player;
     // Start is called before the first frame update
     void Start()
     {
-        
+        player = GameObject.Find("Player");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject == player)
+        {
+            collision.gameObject.GetComponent<PlayerController>().currentSobarValue += 10;
+        }
     }
 }
