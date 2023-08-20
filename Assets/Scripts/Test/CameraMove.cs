652caa4c6736bf3f15df8c6b4e4c26bef0a216cb
37 <73333333@qq.com>
2023-08-21 00:43:57 +0800

    转场动画

--- Assets/Scripts/Test/CameraMove.cs
+++ Assets/Scripts/Test/CameraMove.cs
@@ -42,21 +42,75 @@ public class CameraMove : MonoBehaviour
 
     public float speed = 1;
 
+    public int scalespeed = 5;
+
+    public float m_FieldOfView_1;
+
+    public float m_FieldOfView_2;
+
+    //public AudioClip[] audioClips;
+
+    private void Start()
+    {
+        m_FieldOfView_1 = 70;
+        m_FieldOfView_2 = 43;
+
+    }
+
 
     private void Update()
     {
-       
-
-
+        
         cm.transform.localPosition = Vector3.Lerp(cm.transform.localPosition, point[count].localPosition, speed * Time.deltaTime);
 
-        if (count<point.Length)
+        if (count<point.Length-1)
         {
             if (Input.GetMouseButtonDown(0) && (Vector2.Distance(cm.transform.position, point[count].transform.localPosition) < 0.5f))
             {
                 count++;
             }
+
+            if (count == 1)
+            {
+                //this.GetComponent<AudioSource>().clip = audioClips[count];
+            }
+            if (count == 2)
+            {
+
+            }
+            if (count == 3)
+            {
+
+            }
+
+            if (count == 4)
+            {
+
+                if (Camera.main.fieldOfView < m_FieldOfView_1)
+                {
+                    Camera.main.fieldOfView += (Time.deltaTime * scalespeed);
+                }
+            }
+
+            if (count == 5)
+            {
+                if (Camera.main.fieldOfView > m_FieldOfView_2)
+                {
+                    Camera.main.fieldOfView -= (Time.deltaTime * scalespeed);
+                }
+            }
+
+            if (count == 6)
+            {
+
+            }
+            if (count == 7)
+            {
+
+            }
         }
+
+       
 
     }
 
