ae6cfdfbacbc8d0475a5ea91563d4dd8b613d9a2
37 <73333333@qq.com>
2023-08-21 02:46:42 +0800

    过场动画以及音效

--- Assets/Scripts/Test/CameraMove.cs
+++ Assets/Scripts/Test/CameraMove.cs
@@ -3,36 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraMove : MonoBehaviour
-{
-
-    //public Camera cm;
-
-    //public Transform[] point;
-
-    //private int count = 1;
-
-    //public int speed = 1;
-
-
-    //private void Update()
-    //{
-    //    if (count < point.Length)
-    //    {
-    //        if (Input.GetMouseButtonDown(0))
-    //        {
-    //            Click();
-    //        }
-
-    //    }
-    //}
-
-
-    //public void Click()
-    //{
-    //    cm.transform.localPosition = point[count].transform.localPosition;
-    //    count++;
-    //}
-
+{ 
 
     public Camera cm;
 
@@ -48,7 +19,7 @@ public class CameraMove : MonoBehaviour
 
     public float m_FieldOfView_2;
 
-    //public AudioClip[] audioClips;
+    public AudioClip[] audioClips;
 
     private void Start()
     {
@@ -60,36 +31,32 @@ public class CameraMove : MonoBehaviour
 
     private void Update()
     {
-        
+        //this.GetComponent<AudioSource>().clip = audioClips[count];
         cm.transform.localPosition = Vector3.Lerp(cm.transform.localPosition, point[count].localPosition, speed * Time.deltaTime);
 
-        if (count<point.Length-1)
+        if (count<point.Length-2)
         {
             if (Input.GetMouseButtonDown(0) && (Vector2.Distance(cm.transform.position, point[count].transform.localPosition) < 0.5f))
             {
                 count++;
+                this.GetComponent<AudioSource>().clip = audioClips[0];
+                this.GetComponent<AudioSource>().Play();
             }
-
-            if (count == 1)
-            {
-                //this.GetComponent<AudioSource>().clip = audioClips[count];
-            }
-            if (count == 2)
-            {
-
-            }
-            if (count == 3)
-            {
-
-            }
+            //if (count ==1)
+            //{
+            //    this.GetComponent<AudioSource>().clip = audioClips[2];
+            //    this.GetComponent<AudioSource>().Play();
+            //}
 
             if (count == 4)
             {
 
                 if (Camera.main.fieldOfView < m_FieldOfView_1)
                 {
-                    Camera.main.fieldOfView += (Time.deltaTime * scalespeed);
+                    Camera.main.fieldOfView += (Time.deltaTime * scalespeed*1.5f);
                 }
+                //this.GetComponent<AudioSource>().clip = audioClips[1];
+                //this.GetComponent<AudioSource>().Play();
             }
 
             if (count == 5)
@@ -100,14 +67,7 @@ public class CameraMove : MonoBehaviour
                 }
             }
 
-            if (count == 6)
-            {
-
-            }
-            if (count == 7)
-            {
-
-            }
+            
         }
 
        
